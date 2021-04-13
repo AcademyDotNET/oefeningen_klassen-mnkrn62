c58663be760d48e9f562a0cb29d7917b21688672
Monika rana <mnkrn62@gmail.com>
2021-04-13 10:20:39 +0200

    Init

---
+++ Classesexcersies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Classesexcersies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
