746e0c67650a0baec3d5f061a2ff933f16ef655e
Monika rana <mnkrn62@gmail.com>
2021-04-13 10:29:30 +0200

    Changed the console.writeline again

--- Classesexcersies/Program.cs
+++ Classesexcersies/Program.cs
@@ -6,7 +6,7 @@ namespace Classesexcersies
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! This works.");
+            Console.WriteLine("Hello World! This works.´d");
         }
     }
 }
